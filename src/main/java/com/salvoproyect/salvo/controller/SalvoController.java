3bb90b7ce59f9b66f9276d35e74bb8f179b2bc1d
Renato <renato.reyes.valencia@gmail.com>
2021-06-18 18:44:55 -0400

    make it better

--- src/main/java/com/salvoproyect/salvo/controller/SalvoController.java
+++ src/main/java/com/salvoproyect/salvo/controller/SalvoController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SalvoController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "redirect:/web";
+    }
+
     @RequestMapping("/web")
     public String inicio(){
         return "games";
